92824ffe52d70fe52cf46c110061ad78c15c06f1
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-06-26 19:40:59 -0300

    Tentando corrigir bug no CORS
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- src/main/java/me/dio/controller/GameController.java
+++ src/main/java/me/dio/controller/GameController.java
@@ -4,14 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,6 +16,7 @@ import me.dio.service.GameService;
 
 @RestController
 @RequestMapping("/games")
+@CrossOrigin
 @Tag(name = "Game Controller", description = "APIs for managing games")
 public class GameController {
 
