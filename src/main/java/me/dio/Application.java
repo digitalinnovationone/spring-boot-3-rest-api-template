2df84c42b8b51b71696d9e52a4b33459d1c532e1
Venilton FalvoJr <falvojr@gmail.com>
2023-07-04 19:11:33 -0300

    Improvement JavaDoc

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -7,9 +7,15 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 /**
- * About use HTTPS in Swagger UI:
+ * Initializes our RESTful API.
+ * 
+ * <p>
+ * The {@link OpenAPIDefinition} annotation was used to enable HTTPS in the Swagger UI.
+ * For more details, see the following post on Stack Overflow: 
  * https://stackoverflow.com/a/71132608/3072570
+ * </p>
  */
+
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
