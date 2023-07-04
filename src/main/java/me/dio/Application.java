3e1af524e506f89067efc9dd6557fb7a6bb8ff24
Venilton FalvoJr <falvojr@gmail.com>
2023-07-04 17:18:41 -0300

    Try to fix HTTPS issue on Swagger UI

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -3,6 +3,14 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+/**
+ * About use HTTPS in Swagger UI:
+ * https://stackoverflow.com/a/71132608/3072570
+ */
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
